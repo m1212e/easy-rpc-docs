bf5dfedc819a63375c7e1196d7598503500cdeea
m1212e <->
2022-05-11 12:19:45 +0200

    Updated link

--- src/components/Navbar/Entry.svelte
+++ src/components/Navbar/Entry.svelte
@@ -1,8 +1,8 @@
 <script>
 </script>
 
-<a href="/">
-    <div class="py-2 px-3 m-1 rounded-md hover:bg-slate-200 duration-200 w-100">
-        <slot />
-    </div>
+<a href="/easy-rpc-docs">
+	<div class="py-2 px-3 m-1 rounded-md hover:bg-slate-200 duration-200 w-100">
+		<slot />
+	</div>
 </a>
