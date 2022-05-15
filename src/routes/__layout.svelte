f1353ecc1394e7530f8a0ea7e116307061b213b9
m1212e <->
2022-05-16 00:27:10 +0200

    Fixed index route

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,4 +1,5 @@
 <script context="module">
+	import { dev } from '$app/env';
 	import Navbar from '../components/Navbar/Navbar.svelte';
 	import Search from '../components/Search/Search.svelte';
 	import DarkModeToggle from '../components/DarkModeToggle.svelte';
@@ -17,9 +18,11 @@
 </script>
 
 <div class:dark={$darkModeActive} class="h-screen">
-	<div class="bg-white dark:bg-gray-900 text-black dark:text-gray-300 duration-200 grid-container h-full">
+	<div
+		class="bg-white dark:bg-gray-900 text-black dark:text-gray-300 duration-200 grid-container h-full"
+	>
 		<span class="p-3 flex justify-between items-center head">
-			<a href="/"><span class="text-3xl font-bold">easy-rpc</span></a>
+			<a href={dev ? '/' : '/easy-rpc-docs'}><span class="text-3xl font-bold">easy-rpc</span></a>
 			<span class="flex items-center space-x-4">
 				<a href="https://github.com/m1212e/easy-rpc"><GithubIcon /></a>
 				<DarkModeToggle />
